e8b387961a8a3602e6e44440cf954688b2457473
Gabriel Vinícius <viniciusgsa99@gmail.com>
2022-09-01 23:29:31 -0300

    Atualizando página Projects.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import '../css/Projects.css';
 
 export default function Projects() {
+  const [repos, setRepos] = useState([]);
+
   useEffect(() => {
     document.title = 'Projects';
+    const apiGitHub = async () => {
+      const reposArray = await fetch('https://api.github.com/users/VGabriel-7/repos')
+        .then((promise) => promise.json());
+      setRepos(reposArray);
+    };
+    apiGitHub();
   }, []);
 
-  return <h1>Project</h1>;
+  return (
+    <div className="containerProjects">
+      <h1 className="project-title">Projects</h1>
+      <section className="github-projects">
+        {repos.length > 0 ? repos.map((repo) => (
+          <div
+            className="container-repo"
+            key={ repo.name }
+          >
+            <a
+              className="link-repo"
+              href={ repo.html_url }
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              { repo.name }
+            </a>
+          </div>
+        )) : <p>Loading...</p>}
+      </section>
+    </div>
+  );
 }
